270766399bd373b18f5070336edc1e2d0c1e3ec2
EsamKhalid <esam-khalid@outlook.com>
2024-03-31 20:36:24 +0100

    started to implement sorting

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
-function test(){
-    var str = document.getElementById('test').value;
-    str.innerHTML = "test";
-}
 
-function sortTest(){
-    
+function sortElements(){
+    const list = document.getElementById("column");
+    const sortableArr = document.getElementsByClassName("sortable");
+    const selectValue = document.getElementById('sort-by').value;
+
+    console.log(list);
+
+    const dates = [];
+
+    //populate dates array
+    for(let i = 0; i < sortableArr.length; i++){
+        dates.push(new Date(sortableArr.item(i).getAttribute("data-date")).getTime());
+    }
+
+    //ascending
+    if(selectValue == "ascending"){
+        dates.sort(function(a,b){return a - b});
+    }
+    //descending
+    else if(selectValue == "descending"){
+        dates.sort(function(a,b){return b - a});
+        
+    }
+
+    const tempArr = [];
+
+    for(let i = 0; i < sortableArr.length; i++){
+        
+        const d = new Date(sortableArr.item(i).getAttribute("data-date"));
+        //gets index of selected item
+        const dindex = dates.indexOf(d.getTime());
+        //if the index is already populated (another element with the same date)
+        if(tempArr[dindex] != undefined){
+            //add it into the index above
+            tempArr[dindex + 1] = sortableArr.item(i);
+        }
+        else{
+            tempArr[dindex] = sortableArr.item(i);
+        }
+        
+    }
+
+    //appends the sorted items in order into the list div
+    for(let i = 0; i < dates.length; i++){
+        if(tempArr[i] != undefined){
+            list.appendChild(tempArr[i]);
+            
+        }
+    }
 }
